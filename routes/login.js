db5469ceeda126c717df8bf575d2e38c51995cac
WangShayne <1614565666@qq.com>
2016-09-28 13:32:58 +0800

    updata

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/login');
+//login页面
+router.get('/',function(req,res,next){
+  res.render('login',{title:"请登录"})
 });
 
 module.exports = router;
